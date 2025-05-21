ef997fbb10b45da04a8b1281a37fbc53811da8eb
Arda Ozan Yildiz <6242559@collegemv.qc.ca>
2025-05-21 15:51:43 -0400

    correction

--- IdeaManager.Tests/Services/IdeaServiceTests.cs
+++ IdeaManager.Tests/Services/IdeaServiceTests.cs
@@ -17,35 +17,41 @@ namespace IdeaManager.Tests.Services
 
         public IdeaServiceTests()
         {
-            // creation dun fake repo avec un Idea
-            //je crée un Fake IUnitOfWork qui utilise mon fake repo et non ma bd
-            // Configure SaveChangesAsync() pour retourner 1 (nbr de lignes affectees)
-            //j'instancie le ideaService en lui passant mon faux mock pr tester la logique metier (logique de si tout fonctionne bien)
+            // creation dun fake repo a base de Idea
             _repoMock = new Mock<IRepository<Idea>>();
+
+            //je crée un Fake IUnitOfWork qui utilise mon fake repo et non ma bd
             _uowMock = new Mock<IUnitOfWork>();
             _uowMock
                 .Setup(u => u.IdeaRepository)
                 .Returns(_repoMock.Object);
+
+            // Configure SaveChangesAsync() pour retourner 1 (nbr de lignes affectees)
             _uowMock
                 .Setup(u => u.SaveChangesAsync())
                 .ReturnsAsync(1);
+
+            //j'instancie le ideaService en lui passant mon faux mock pr tester la logique metier (logique de si tout fonctionne bien)
             _service = new IdeaService(_uowMock.Object);
         }
 
         [Fact]
         public async Task SubmitIdeaAsync_EmptyTitle_ThrowsArgumentException()
         {
-            //
-            var test = new Idea
+            //technique des trois A
+            // Arrange: idee mauvaise (je mets un titre vide)
+            var pasDeTitre = new Idea
             {
-                Title = "test",
-                Description = "cela est un test unitaire"
+                Title = "",
+                Description = "Cela est un test"
             };
 
-            
+            // Act & Assert: l'appel doit jeter ArgumentException
             await Assert.ThrowsAsync<ArgumentException>(
-                () => _service.SubmitIdeaAsync(test)
+                () => _service.SubmitIdeaAsync(pasDeTitre)
             );
+
+            // Et on ne doit JAMAIS appeler AddAsync ni SaveChangesAsync
             _repoMock.Verify(r => r.AddAsync(It.IsAny<Idea>()), Times.Never);
             _uowMock.Verify(u => u.SaveChangesAsync(), Times.Never);
         }
